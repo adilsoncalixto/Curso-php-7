a6f70c68f0981c6e49d2ceb64236b9c4d352629e
Alexandresl <alexandre@alsites.com.br>
2017-12-27 02:14:26 -0200

    Aula 13 - Utilizando variáveis com PHP

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+    // cria a variável
+    $nome = "Alexandre";
+    
+    // imprimi na tela
+    echo $nome;
+    
+    echo "</br>";
+    
+    /* 
+    / var_dump() - Esta função mostrará uma representação estruturada sobre uma ou mais expressões, /incluindo o tipo e o valor. Arrays e objetos são explorados recursiamente com valores identados /na estrutura mostrada. 
+   */
+    var_dump($nome);
+?>
