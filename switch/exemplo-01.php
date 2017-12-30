54232cb4c39f254444bb3e44cb636f6273e7f208
Alexandresl <alexandre@alsites.com.br>
2017-12-30 14:20:25 -0200

    Aula 21 - Switch Case

---
+++ switch/exemplo-01.php
@@ -0,0 +1,31 @@
+<?php
+    // date() - Recupera dia da semana retornando um int de 0 a 6
+    $diaDaSemana = date("w");
+    // utilizamos o switch para exibir o nome ao usuário final
+    switch ($diaDaSemana) {
+        case 0:
+            echo "Domingo";
+            break;
+        case 1:
+            echo "Segunda-Feira";
+            break;
+        case 2:
+            echo "Terça-Feira";
+            break;
+        case 3:
+            echo "Quarta-Feira";
+            break;
+        case 4:
+            echo "Quinta-Feira";
+            break;
+        case 5:
+            echo "Sexta-Feira";
+            break;
+        case 6:
+            echo "Sábado";
+            break;
+        default:
+            echo "Data inválida.";
+            break; // opcional no último case
+    }    
+?>
