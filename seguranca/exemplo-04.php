7318da3de5e9f050b5985b82afa257b62b2d0de3
Alexandre <alexandre@alsites.com>
2018-01-06 13:21:58 -0200

    Aula 92 - Usando o reCaptcha para impedir Robôs

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,6 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<form action="cadastrar.php" method="post">
+	<div class="g-recaptcha" data-sitekey="6Lcshz8UAAAAABC2nwEBOoPJlUOvFpG9GkiANC1X"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+</form>
